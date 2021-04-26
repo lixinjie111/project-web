bc0cdd4647eceff7ce41860a44c02630a4d5966d
露露 <18330276226@163.com>
2021-04-26 18:29:56 +0800

    ui依赖

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
+import Xui from 'x-intelligent-ui'
+import 'x-intelligent-ui/dist/main.css'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 
 Vue.config.productionTip = false
 
+Vue.use(Xui)
 Vue.use(Vuex)
 Vue.use(VueRouter)
 
