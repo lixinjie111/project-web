5c5168614d63d0106ab8d72df94792147ffdc2d2
hd <danmn1230@sina.com>
2021-05-18 16:57:11 +0800

    UI组件中文包设置

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <a-locale-provider :locale="locale">
+        <div id="app">
+            <router-view></router-view>
+        </div>
+    </a-locale-provider>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    import zhCN from 'x-intelligent-ui/lib/locale-provider/zh_CN';
+    export default {
+        name: 'App',
+        data() {
+            return {
+                locale: zhCN,
+            };
+        },
+    };
 </script>
